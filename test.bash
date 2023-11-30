ff7ef5054a7c7cdfd926b1fed8e09e3c59f60d75
Yuna Hayashi <s22c1105lv@s.chibakoudai.jp>
2023-11-30 18:39:41 +0900

    Add a woekflows

--- test.bash
+++ test.bash
@@ -1,5 +1,24 @@
 #!/bin/bash -xv
 
+ng () {
+	echo NG at Line $1
+	res= 1
+}
+
+res= 0
+
 out=$(seq 5 | ./plus)
 
-["${out}" = 15]
+["${out}" = 15] || ng $LINENO
+
+out=$(echo あ | ./plus)
+[ "$?" = 1] || ng $LINENO
+[ "${out}" = "" ] || ng $LINENO
+
+out=$(echo | ./plus)
+[ "$?" = 1] || ng $LINENO
+[ "${out}" = "" ] || ng $LINENO
+
+[ "$res" = 0 ] && echo OK
+
+exit $res
