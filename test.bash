12e66bcb440ac17df820c6cbd89000ea0e244bf1
Yuna Hayashi <s22c1105lv@s.chibakoudai.jp>
2023-11-30 21:20:39 +0900

    souwa

--- test.bash
+++ test.bash
@@ -1,24 +1,17 @@
 #!/bin/bash
+# SPDX-FileCopyrighiText: 2022 Yuna Hayashi s22c1105lv@s.chibakoudai.jp
+# SPDX-License-Identifier: BSD-3-Clause
 
 ng () {
 	echo NG at Line $1
-	res= 1
+	res=1
 }
 
-res= 0
+res=0
 
 out=$(seq 5 | ./plus)
 
-["${out}" = 15] || ng $LINENO
-
-out=$(echo あ | ./plus)
-[ "$?" = 1] || ng $LINENO
-[ "${out}" = "" ] || ng $LINENO
-
-out=$(echo | ./plus)
-[ "$?" = 1] || ng $LINENO
-[ "${out}" = "" ] || ng $LINENO
+[ "$out" = 15 ] || ng $LINENO
 
 [ "$res" = 0 ] && echo OK
-
 exit $res
